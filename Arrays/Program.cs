8af7b51996853b5c05c527b33ad1010e389cd446
karburaator <example@mail.com>
2024-10-12 21:08:37 +0300

    week 6

---
+++ Arrays/Program.cs
@@ -0,0 +1,18 @@
+﻿
+string[] fruitBasket = { "apples", "bananas", "oranges" };
+
+Console.WriteLine($"There are {fruitBasket.Length} fruit in your basket.");
+
+for (int i = 0; i < fruitBasket.Length; i++)
+{
+    Console.WriteLine(fruitBasket[i]);
+}
+
+fruitBasket[0] = "pine-apple";
+fruitBasket[2] = "peaches";
+
+
+for(int i = 0; i < fruitBasket.Length; i++)
+{
+    Console.WriteLine(fruitBasket[i]);
+}
---
+++ Week6StringsAndArrays/Program.cs
@@ -0,0 +1,27 @@
+﻿
+string userName = "Darth Vader";
+
+Console.WriteLine(userName.Length);
+Console.WriteLine($"The last letter in your name is {userName[userName.Length - 1]}");
+
+//replace
+//a - 4
+
+string updatedUserName = userName.Replace('a', '4').Replace('e', '3');
+
+Console.WriteLine(updatedUserName);
+
+
+//a-counter
+
+//int counter = 0;
+
+//for ( int i = 0; i < userName.Length; i++ )
+//{
+//    if(userName[i] == 'a')
+//    {
+//        counter++; //counter = counter + 1
+//    }
+//}
+
+//Console.WriteLine($"Your first name has {counter} a-letters.");
